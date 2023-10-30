523f2d2528e65c5c4d196fd7cc1d85490f636bba
guofeng <one778@163.com>
2023-10-30 17:17:05 +0800

    feat: add LinkedList pages

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,4 +2,10 @@
 module.exports = [
   { text: '首页', link: '/' },
   {text: 'LeetCode', link: '/leetcode/'},
+  {text: 'CsSystem',
+     items: [{
+      text: 'Algorithm',
+      link: '/cs/algorithm/'
+    }]
+  }
 ]
